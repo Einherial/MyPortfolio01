a66add08093d67ecc60bc435a933aed5e466ed0b
Einherial <ylivesylike@gmail.com>
2019-04-11 15:44:16 +0900

    test my portfolio

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+  <meta charset = "utf-8">
+  <title>YMN.space</title>
+  <link rel="stylesheet" href="stylesheet.css">
+</head>
+
+<body>
+  <div class="header">
+  </div>
+
+
+
+  <div class="main">
+  </div>
+
+
+
+  <div class="footer">
+  </div>
+
+</body>
+</html>
