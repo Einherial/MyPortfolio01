244582d03b41f1d09b5254008f0ec0c78761e151
Einherial <ylivesylike@gmail.com>
2019-04-12 00:31:08 +0900

    update

--- index.php
+++ index.php
@@ -3,22 +3,45 @@
 <head>
   <meta charset = "utf-8">
   <title>YMN.space</title>
-  <link rel="stylesheet" href="stylesheet.css">
+  <link rel="shortcut icon" href="./img/favicon.ico" type=".img/image/vnd.microsoft.icon">
+  <link rel="stylesheet" href="./stylesheets/stylesheet.css">
 </head>
 
+
 <body>
-  <div class="header">
+  <div id="wrap">
+    <!--ヘッダー開始-->
+    <header>
+
+    </header>
+    <!--ヘッダー終了-->
+
+
+    <!--上段１開始-->
+    <div class="top-wrapper">
+        <!--背景透過のための空タグ-->
+        <div class="bgimg"></div>
+        <!-- <div class="container"> -->
+          <h1>YMN.space</h1>
+          <p>NAMEのポートフォリオサイトです。</p>
+        </div>
+    </div>
+  <!--上段１終了-->
+
+
+    <!--上段２開始-->
+    <div class="middle-wrapper">
+      <p>自己紹介</p>
+      <p>自己紹介</p>
+      <p>自己紹介</p>
+      <p>自己紹介</p>
+    </div>
+    <!--上段２終了-->
+
+    <!--フッター開始-->
+    <footer>
+    </footer>
+    <!--フッター終了-->
   </div>
-
-
-
-  <div class="main">
-  </div>
-
-
-
-  <div class="footer">
-  </div>
-
 </body>
 </html>
